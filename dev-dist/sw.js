69b5928a4e686e077f816c55c1eef9b19ac842c7
robin.boeckmann <robin.boeckmann@flixbus.com>
2023-11-29 23:30:54 +0100

    add icons in manifest

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-8cc11778'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.1s88ho297t"
+    "revision": "0.s2tn1r0a6t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
